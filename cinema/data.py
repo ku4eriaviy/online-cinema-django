ee3e341fcfa5a7c6a659efcd5da2945ba6cb6ea7
ku4eriaviy <kozinegor10@gmail.com>
2025-10-29 19:27:22 +0500

    Добавлены папки templates, static и данные о фильмах

---
+++ cinema/data.py
@@ -0,0 +1,15 @@
+
+MOVIES = [
+    {"title": "Интерстеллар", "genre": "sci-fi", "year": 2014, "poster": "interstellar.jpg"},
+    {"title": "Побег из Шоушенка", "genre": "drama", "year": 1994, "poster": "shawshank.jpg"},
+    {"title": "Матрица", "genre": "sci-fi", "year": 1999, "poster": "matrix.jpg"},
+    {"title": "Крёстный отец", "genre": "drama", "year": 1972, "poster": "godfather.jpg"},
+    {"title": "Начало", "genre": "sci-fi", "year": 2010, "poster": "inception.jpg"},
+    {"title": "Форрест Гамп", "genre": "drama", "year": 1994, "poster": "forrest.jpg"},
+]
+
+GENRES = {
+    "all": "Все жанры",
+    "sci-fi": "Научная фантастика",
+    "drama": "Драма",
+}
