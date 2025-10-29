a09983d4831f483f9602196b925346aa6bdfe099
ku4eriaviy <kozinegor10@gmail.com>
2025-10-29 19:34:28 +0500

    Добавлено представление home с фильтрацией и cookie

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .data import MOVIES, GENRES
 
-# Create your views here.
+def home(request):
+    # Получаем выбранный жанр из URL (например: ?genre=sci-fi)
+    selected_genre = request.GET.get('genre', 'all')
+    
+    # Фильтруем фильмы
+    if selected_genre == 'all':
+        filtered_movies = MOVIES
+    else:
+        filtered_movies = [movie for movie in MOVIES if movie['genre'] == selected_genre]
+
+    # Рендерим шаблон и передаём данные
+    response = render(request, 'index.html', {
+        'movies': filtered_movies,
+        'genres': GENRES,
+        'selected_genre': selected_genre,
+    })
+    
+    # Сохраняем выбор в cookie на 30 дней
+    response.set_cookie('last_genre', selected_genre, max_age=30*24*60*60)
+    
+    return response
